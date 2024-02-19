4f63f638725cb7dc7c1ed7fa7b1e136aefc45614
Alexander Galev <alex.galev@yahoo.com>
2024-02-19 10:48:47 -0800

    utilized polars to upload 2 datasets to an AWS database

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import polars as pl
 
 load_dotenv()
 
-# from datetime import datetime
+db = os.getenv('db')
 
 # df_csv = pl.read_csv('healthkit_heartrate.csv')
 # print(df_csv)
@@ -16,7 +16,15 @@ load_dotenv()
 # # out.write_csv('healthkit_heartrate.csv')
 # out.write_json('healthkit_heartrate.json')
 
-db = os.getenv('db')
-query = "SELECT * FROM competitions"
+# READ FROM DATABASE
+# query = "SELECT * FROM healthkit_heartrate"
+# print(pl.read_database_uri(query=query, uri=db, engine='adbc'))
 
-print(pl.read_database_uri(query=query, uri=db, engine='adbc'))
+# Expanded Print
+# with pl.Config(tbl_cols=-1):
+#     print(out)
+
+# WRITE TO DATABASE
+# df = pl.read_json('healthkit_workouts.json').unnest('metadata')
+# out = df.select(pl.col('*').exclude(pl.Null)).sort('start')
+# out.write_database(table_name='healthkit_workouts', connection=db, engine='adbc')
