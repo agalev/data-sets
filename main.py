2cbf735effa7b9fbc5d028853562761f5569f5ee
Alexander Galev <alex.galev@yahoo.com>
2024-02-18 17:33:30 -0800

    Established connection with database, added .env and .gitignore, swapped db driver to ADBC

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
+from dotenv import load_dotenv
 import polars as pl
+
+load_dotenv()
+
 # from datetime import datetime
 
-df_csv = pl.read_csv('healthkit_heartrate.csv')
-print(df_csv)
+# df_csv = pl.read_csv('healthkit_heartrate.csv')
+# print(df_csv)
 
 # out = dataframe.select([
 #     "startDate",
@@ -10,3 +15,8 @@ print(df_csv)
 # ])
 # # out.write_csv('healthkit_heartrate.csv')
 # out.write_json('healthkit_heartrate.json')
+
+db = os.getenv('db')
+query = "SELECT * FROM competitions"
+
+print(pl.read_database_uri(query=query, uri=db, engine='adbc'))
